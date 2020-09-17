89fae302ef424d63e8dee7c93a22c15a9d51c505
Nur Cetin <seymanur4450@gmail.com>
2020-09-16 19:25:09 -0500

    Completed the Job Class.

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -29,14 +29,14 @@ public class PositionType {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PositionType that = (PositionType) o;
-        return id == that.id &&
-                value.equals(that.value);
+        return id == that.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, value);
+        return Objects.hash(id);
     }
+
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
